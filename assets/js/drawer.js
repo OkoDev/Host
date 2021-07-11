b3736b222c0e5fb271ca9bc363ddaed17dfd7317
okodev <oko.webdev@gmail.com>
2021-07-11 16:22:23 +0300

    fixed: README.md add: favicon.ico

--- assets/js/drawer.js
+++
@@ -1,9 +0,0 @@
-const drawer = document.querySelector('.menu__btn');
-const menu = document.querySelector('.menu__box');
-
-
-drawer.addEventListener('click', (event) => {
-    drawer.classList.toggle('active');
-    menu.classList.toggle('active');
-
-})
